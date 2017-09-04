74c239bc95e9214f4ff8e7ecb28ddaa7d68a1fca
Njeri Kieha <sylvia.kieha@andela.com>
2017-09-05 01:03:38 +0300

    Use mongoose to create journal model

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-/* eslint-disable no-tabs */
 /* eslint-disable no-console */
 
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -12,14 +12,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-	res.send("Hello World");
+  res.send("Hello World");
+});
+
+mongoose.connect("mongodb://localhost:27017/journalApp", (err) => {
+  if (err) {
+    console.log("An error occured while connecting to the database:", err.message);
+  } else {
+    console.log("Database connection successful");
+  }
 });
 
 app.listen(8080, (err) => {
-	if (err) {
-		console.log("An error occured while connecting:", err);
-	}
-	console.log("App listening 👂🏽 on port 8080");
+  if (err) {
+    console.log("An error occured while connecting:", err);
+  }
+  console.log("App listening 👂🏽 on port 8080");
 });
 
 module.exports = app;
---
+++ server/models/journal.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const JournalSchema = mongoose.Schema({
+  title: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  entry: {
+    type: String,
+    required: true,
+  },
+},
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model("Journal", JournalSchema);
