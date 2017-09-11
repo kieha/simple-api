11c375c8c0dc8294fcd6ae4ffcb09233ff099988
Njeri Kieha <sylvia.kieha@andela.com>
2017-09-11 07:07:19 +0300

    Webpack finishing touches, React basic component

---
+++ client/components/app.js
@@ -0,0 +1,11 @@
+/* eslint-disable react/jsx-filename-extension */
+
+import React from "react";
+
+const App = () => (
+  <div style={{ textAlign: "center" }}>
+    <h1>Hello World</h1>
+  </div>
+);
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,11 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: "./client/index.html",
+  filename: "index.html",
+  inject: "body",
+});
 
 module.exports = {
   entry: "./client/index.js",
@@ -8,8 +15,8 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: "babel-loader", exclude: /node_modules/ },
       { test: /\.jsx$/, loader: "babel-loader", exclude: /node_modules/ },
     ],
   },
+  plugins: [HtmlWebpackPluginConfig],
 };
