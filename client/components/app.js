854d0f4a3aad281efe18e50f9735001ebf2a22a3
Njeri Kieha <sylvia.kieha@andela.com>
2017-09-13 03:51:36 +0300

    Use bootstrap components

--- client/components/app.js
+++ client/components/app.js
@@ -1,11 +1,52 @@
 /* eslint-disable react/jsx-filename-extension */
 
-import React from "react";
+import React, { Component } from "react";
 
-const App = () => (
-  <div style={{ textAlign: "center" }}>
-    <h1>Hello World</h1>
-  </div>
-);
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      entry: "",
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="jumbotron jumbotron-fluid" style={{ textAlign: "center" }}>
+          <h1>Jot It Down</h1>
+
+          <div className="container" style={{ paddingTop: "46px" }}>
+
+            <form noValidate>
+              <div className="form-group">
+                <label htmlFor>Title</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Journal Entry Title"
+                  required
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor>Journal Entry</label>
+                <textarea
+                  className="form-control"
+                  rows="5"
+                />
+              </div>
+
+              <button type="submit" className="btn btn-primary">Submit Journal Entry</button>
+            </form>
+
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
