7fa40d14dd16bd34062b183bd70dceaaffae5661
Njeri Kieha <sylvia.kieha@andela.com>
2017-09-17 23:41:15 +0300

    Rendering journal list

--- client/components/app.js
+++ client/components/app.js
@@ -1,8 +1,10 @@
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable no-undef */
+/* eslint-disable no-underscore-dangle */
 
 import React, { Component } from "react";
 import request from "superagent";
+import Moment from "react-moment";
 
 class App extends Component {
 
@@ -11,10 +13,31 @@ class App extends Component {
     this.state = {
       title: "",
       entry: "",
+      journalEntries: [],
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchEntries();
+  }
+
+  fetchEntries() {
+    request
+      .get("http://localhost:3000/api/journals")
+      .end((err, res) => {
+        if (res.body.message) {
+          this.setState({
+            journalEntries: [],
+          });
+        } else {
+          this.setState({
+            journalEntries: res.body,
+          });
+        }
+      });
   }
 
   handleInputChange(event) {
@@ -53,8 +76,27 @@ class App extends Component {
               entry: "",
             });
           });
+          this.fetchEntries();
         }
       });
+  }
+
+  renderJournalList() {
+    return this.state.journalEntries.length ?
+      this.state.journalEntries.map(entry => (
+        <div className="list-group" key={entry._id} style={{ marginTop: 20, marginBottom: 20 }}>
+          <div className="list-group-item flex-column align-items-start" style={{ textAlign: "center", justifyContent: "center" }}>
+            <h3>{entry.title}</h3>
+            <h6>Created at: {<Moment format="HH:mm DD/MM/YYYY">{entry.createdAt}</Moment>}</h6>
+            <p>{entry.entry}</p>
+          </div>
+        </div>
+      ))
+      :
+      <div className="not-found" style={{ marginTop: 20, marginBottom: 20 }}>
+        <h3 style={{ textAlign: "center" }}>No journal entries yet!</h3>
+        <h4 style={{ textAlign: "center" }}>Insert an entry in the form above to get started</h4>
+      </div>;
   }
 
   render() {
@@ -95,6 +137,11 @@ class App extends Component {
             </form>
 
           </div>
+        </div>
+
+        <div className="container-fluid">
+          <h2 style={{ textAlign: "center" }}>Journal Entries</h2>
+          {this.renderJournalList()}
         </div>
       </div>
     );
